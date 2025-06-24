ca02cef04c533af83082f4cf1a3f962ce8ebb608
ravi <ravichandu.b1409@gmail.com>
2025-06-24 13:01:10 -0400

    made changes to page.tsx due to errors in build

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
 
-export default async function ProductPage(
-  { params }: { params: { id: string } } 
-) {
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ["default_price"],
-    });
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
+};
 
-    const plainProduct = JSON.parse(JSON.stringify(product));
-    return <ProductDetail product={plainProduct} />;
+export default async function ProductPage({ params }: ProductPageProps) {
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"],
+  });
+
+  const plainProduct = JSON.parse(JSON.stringify(product));
+  return <ProductDetail product={plainProduct} />;
 }
